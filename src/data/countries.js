4a61e45ab07bf985ec04b3da2247bd4c365db04c
pdaloxd <31321612+pablodrake@users.noreply.github.com>
2026-01-30 09:55:43 +0700

    Fix non existent country
    Deleted Ceuta and "Mulilla" (Melilla) from the countries list as they are not Countries but Spanish cities

--- src/data/countries.js
+++ src/data/countries.js
@@ -815,17 +815,6 @@ export default [
     status: 'assigned',
   },
   {
-    alpha2: 'EA',
-    alpha3: '',
-    countryCallingCodes: [],
-    currencies: ['EUR'],
-    emoji: '🇪🇦',
-    ioc: '',
-    languages: [],
-    name: 'Ceuta, Mulilla',
-    status: 'reserved',
-  },
-  {
     alpha2: 'EC',
     alpha3: 'ECU',
     countryCallingCodes: ['+593'],
